f861d1f9e6d3e7ac516754f3868f540d4b4434dd
Karthik Macherla <kmacher@seas.upenn.edu>
2022-04-19 16:37:17 -0400

    testing python-setup

--- server/database.py
+++ server/database.py
@@ -7,7 +7,8 @@ config = get_config()
 
 SQLALCHEMY_DATABASE_URL = config.database_url
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL or "./sql_test_app.db",
+    connect_args={"check_same_thread": False},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
