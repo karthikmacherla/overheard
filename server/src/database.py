8a6982e9d73fba171a30805cd3525c90d8aa2b61
Karthik Macherla <kmacher@seas.upenn.edu>
2022-08-05 18:41:03 -0400

    improve local dev flow

--- server/src/database.py
+++ server/src/database.py
@@ -6,13 +6,12 @@ from config import get_config
 config = get_config()
 
 SQLALCHEMY_DATABASE_URL = config.database_url
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL or "sqlite:///./sql_test_app.db",
-    pool_size=20,
-)
-engine.execute("CREATE DATABASE IF NOT EXISTS overhearddb")  # create db
-engine.execute("USE overhearddb")  # select new db
+engine = (create_engine(SQLALCHEMY_DATABASE_URL, pool_size=20) if SQLALCHEMY_DATABASE_URL 
+          else create_engine("sqlite:///./sql_test_app.db", connect_args={"check_same_thread": False}))
 
+if SQLALCHEMY_DATABASE_URL:    
+    engine.execute("CREATE DATABASE overhearddb")  # create db
+    engine.execute("USE overhearddb")  # select new db
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
